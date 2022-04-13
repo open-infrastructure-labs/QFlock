d1260159c7bd79253cb446a0a3b45f191ec1f0b1
Peter Puhov <peterpuhov.github@gmail.com>
2022-04-13 06:49:43 -0400

    Added inter dc connection

--- start.sh
+++ start.sh
@@ -3,3 +3,9 @@
 ./scripts/start_dc1.sh
 
 ./scripts/start_dc2.sh
+
+sleep 1
+# Add qflock network between datacenters
+docker network connect qflock-net qflock-spark-dc1
+docker network connect qflock-net qflock-storage-dc2
+
