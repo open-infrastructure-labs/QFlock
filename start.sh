75c16de3d86ffd081bca3391182d1738ee823240
Robert Foley <robfoley972@gmail.com>
2022-04-15 11:55:06 -0400

    Add storage dc1 to qflock-net

--- start.sh
+++ start.sh
@@ -7,5 +7,6 @@
 sleep 1
 # Add qflock network between datacenters
 docker network connect qflock-net qflock-spark-dc1
+docker network connect qflock-net qflock-storage-dc1
 docker network connect qflock-net qflock-storage-dc2
 
